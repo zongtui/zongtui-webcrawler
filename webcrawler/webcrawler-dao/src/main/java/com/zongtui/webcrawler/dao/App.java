9acdfd72b322707b964bd71cff721299ee2509c7
cloudskyme <cloudskyme@163.com>
2015-03-31 12:45:37 +0800

    增加webcrawler-dao，单独剥离出dao层

---
+++ webcrawler/webcrawler-dao/src/main/java/com/zongtui/webcrawler/dao/App.java
@@ -0,0 +1,13 @@
+package com.zongtui.webcrawler.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
